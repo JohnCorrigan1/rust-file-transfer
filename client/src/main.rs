82f66fb60d9bbebed51c7ad25ff6bc722399d688
John Corrigan <jcorrigan322@gmail.com>
2024-07-01 16:33:31 -0700

    handle multi file streams and file destinations

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,31 +1,53 @@
-use std::env;
-
+use std::{env, io};
 use tokio::fs::File;
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+
+// const ONE_MB: usize = 1024 * 1024;
+// const FOUR_MB: usize = 4 * 1024 * 1024;
+const EIGHT_MB: usize = 8 * 1024 * 1024;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let server_addr = env::var("TAURI_UPLOAD_SERVER").expect("could not find TAURI_UPLOAD_SERVER");
-    let mut stream = TcpStream::connect(server_addr).await?;
-    let mut file = File::open("").await?;
+    let server_addr = env::var("FILE_UPLOAD_SERVER").expect("could not find FILE_UPLOAD_SERVER");
+    let mut count = 0;
+    let mut total_time = 0.0;
+    loop {
+        let start = std::time::Instant::now();
+        count += 1;
 
-    let filename = "file.txt";
-    let filename_size = filename.len() as u16;
+        let mut stream = TcpStream::connect(&server_addr).await?;
+        stream.set_nodelay(true)?;
+        println!("Connected to server");
 
-    // Send the filename size and filename
-    stream.write_all(&filename_size.to_be_bytes()).await?;
-    stream.write_all(filename.as_bytes()).await?;
+        let mut file = File::open("file.txt").await?;
 
-    let mut buf = [0; 1024];
+        let filename = "file.txt";
 
-    // Read file data and send it
-    while let Ok(n) = file.read(&mut buf).await {
-        if n == 0 {
-            break;
+        let filename_size = filename.len() as u16;
+        stream.write_all(&filename_size.to_be_bytes()).await?;
+        stream.write_all(filename.trim().as_bytes()).await?;
+
+        let mut buf = vec![0; EIGHT_MB];
+
+        while let Ok(n) = file.read(&mut buf).await {
+            if n == 0 {
+                break;
+            }
+            stream.write_all(&buf[0..n]).await?;
         }
-        stream.write_all(&buf[0..n]).await?;
-    }
 
+        total_time += start.elapsed().as_secs_f64();
+        println!("File sent");
+        println!("File {} took: {}", count, start.elapsed().as_secs_f64());
+        if count == 5 {
+            println!("Total time for 5 files: {}", total_time);
+            println!("Average time over 5 files {}", total_time / 5.0);
+            break;
+        } else {
+            println!("Waiting 5 seconds before sending next file");
+            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+        }
+    }
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,86 @@
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
+
+const EIGHT_MB: usize = 8 * 1024 * 1024;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:6969").await?;
+    let listener = TcpListener::bind("0.0.0.0:6060").await?;
 
     loop {
         let (mut socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
-            let mut buf = [0; 1024];
+            let mut buf = vec![0; EIGHT_MB];
             let elapsed = std::time::Instant::now();
-            // Read the filename size and then filename
+
+            println!("Received connection");
+
             if let Ok(_) = socket.read_exact(&mut buf[0..2]).await {
                 let size = u16::from_be_bytes([buf[0], buf[1]]) as usize;
                 if let Ok(_) = socket.read_exact(&mut buf[0..size]).await {
                     let filename = String::from_utf8_lossy(&buf[0..size]).to_string();
+                    if let Ok(_) = socket.read_exact(&mut buf[0..2]).await {
+                        let file_type = FileType::from_u16(u16::from_be_bytes([buf[0], buf[1]]));
+                        let destination = file_type.file_destination(&mut socket).await;
 
-                    // Open the file for writing
-                    let filename = format!("received/{}", filename);
-                    let mut file = tokio::fs::File::create(filename).await.unwrap();
+                        let filename = format!("/mnt/sdb1/{}/{}", destination, filename);
 
-                    // Read the file data from socket and write to file
-                    while let Ok(n) = socket.read(&mut buf).await {
-                        if n == 0 {
-                            break;
+                        std::fs::create_dir_all(format!("/mnt/sdb1/{}", destination)).unwrap();
+
+                        let mut file = tokio::fs::File::create(filename).await.unwrap();
+
+                        while let Ok(n) = socket.read(&mut buf).await {
+                            if n == 0 {
+                                break;
+                            }
+                            file.write_all(&buf[0..n]).await.unwrap();
                         }
-                        file.write_all(&buf[0..n]).await.unwrap();
                     }
                 }
             }
             println!("File received");
             println!("Elapsed time: {:?}", elapsed.elapsed().as_secs_f64());
         });
+    }
+}
+
+enum FileType {
+    Movie,
+    Show,
+    Image,
+    Document,
+}
+
+impl FileType {
+    fn from_u16(value: u16) -> Self {
+        match value {
+            1 => Self::Movie,
+            2 => Self::Show,
+            3 => Self::Image,
+            4 => Self::Document,
+            _ => panic!("Invalid file type"),
+        }
+    }
+
+    async fn file_destination(&self, socket: &mut TcpStream) -> String {
+        match self {
+            Self::Movie => String::from("Movies"),
+            Self::Show => {
+                let mut buf = vec![0; 2048];
+                socket.read_exact(&mut buf[0..2]).await.unwrap();
+                let show_name_size = u16::from_be_bytes([buf[0], buf[1]]) as usize;
+
+                socket
+                    .read_exact(&mut buf[0..show_name_size])
+                    .await
+                    .unwrap();
+
+                let show_name = String::from_utf8_lossy(&buf[0..show_name_size]).to_string();
+                format!("Shows/{}", show_name)
+            }
+            Self::Image => String::from("Images"),
+            Self::Document => String::from("Documents"),
+        }
     }
 }
